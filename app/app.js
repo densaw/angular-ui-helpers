05c4ce226b367101871a5ab8876535f7281390e4
densaw <sawmkc@gmail.com>
2016-02-10 12:34:39 +0200

    btn

--- app/app.js
+++ app/app.js
@@ -1,6 +1,21 @@
 (function(){
 	'use strict';
 
-	angular.module('app', []);
-	
+	angular
+		.module('app', ['ui.router'])
+		.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+			$urlRouterProvider.otherwise('/');
+
+			$stateProvider
+			.state('/', {
+					abstract: true,
+					url: "/home",
+					templateUrl:"../index.html"
+			})
+			.state('btn', {
+					url: "/btn",
+					templateUrl:"../app/items/button-class-switcher.html"
+			});
+		}]);
 }());
--- app/directives/button-class-switcher.js
+++ app/directives/button-class-switcher.js
@@ -3,7 +3,7 @@
 	'use strict';
 
 	angular
-	.module('app', [])
+	.module('app')
 	.directive('bootstrapBtn', bootstrapBtn);
 
 	function bootstrapBtn(){
